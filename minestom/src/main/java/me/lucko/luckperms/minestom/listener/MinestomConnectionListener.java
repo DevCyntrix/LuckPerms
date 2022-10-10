510e38f72f0f82bedd81804f08f1453fe8deaf7d
DevCyntrix <devcyntrix@hotmail.com>
2022-10-10 17:26:06 +0200

    Copy velocity implementation

--- minestom/src/main/java/me/lucko/luckperms/minestom/listener/MinestomConnectionListener.java
+++ minestom/src/main/java/me/lucko/luckperms/minestom/listener/MinestomConnectionListener.java
@@ -25,6 +25,7 @@
 
 package me.lucko.luckperms.minestom.listener;
 
+import com.velocitypowered.api.event.permission.PermissionsSetupEvent;
 import me.lucko.luckperms.common.config.ConfigKeys;
 import me.lucko.luckperms.common.locale.Message;
 import me.lucko.luckperms.common.locale.TranslationManager;
@@ -32,12 +33,20 @@ import me.lucko.luckperms.common.model.User;
 import me.lucko.luckperms.common.plugin.util.AbstractConnectionListener;
 import me.lucko.luckperms.minestom.LPMinestomPlugin;
 import me.lucko.luckperms.minestom.options.PlayerQueryMap;
+import me.lucko.luckperms.minestom.service.PlayerPermissionProvider;
 import net.kyori.adventure.text.Component;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerPreLoginEvent;
+import net.minestom.server.event.player.PlayerDisconnectEvent;
 import net.minestom.server.event.player.PlayerLoginEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 public class MinestomConnectionListener extends AbstractConnectionListener {
     private final LPMinestomPlugin plugin;
@@ -52,6 +61,51 @@ public class MinestomConnectionListener extends AbstractConnectionListener {
 
         eventManager.addListener(AsyncPlayerPreLoginEvent.class, (this::asyncPreLoginHandler));
         eventManager.addListener(PlayerLoginEvent.class, (this::loginEventHandler));
+        eventManager.addListener(PermissionsSetupEvent.class, (this::onPlayerPermissionsSetup));
+        eventManager.addListener(PlayerDisconnectEvent.class, this::onDisconnect);
+
+    }
+
+    private void onDisconnect(@NotNull PlayerDisconnectEvent e) {
+        // Wait until the last priority to unload, so plugins can still perform permission checks on this event
+        handleDisconnect(e.getPlayer().getUuid());
+    }
+
+    public void onPlayerPermissionsSetup(PermissionsSetupEvent e) {
+        /* Called when the player first attempts a connection with the server.
+           The PermissionsSetupEvent is called for players just before the Login event
+
+           We delay the login here, as we want to cache UUID data before the player is connected to a backend bukkit server.
+           This means that a player will have the same UUID across the network, even if parts of the network are running in
+           Offline mode. */
+
+        if (!(e.getSubject() instanceof Player)) {
+            return;
+        }
+
+        final Player player = (Player) e.getSubject();
+
+        if (this.plugin.getConfiguration().get(ConfigKeys.DEBUG_LOGINS)) {
+            this.plugin.getLogger().info("Processing pre-login for " + player.getUuid() + " - " + player.getUsername());
+        }
+            /* Actually process the login for the connection.
+               We do this here to delay the login until the data is ready.
+               If the login gets cancelled later on, then this will be cleaned up.
+
+               This includes:
+               - loading uuid data
+               - loading permissions
+               - creating a user instance in the UserManager for this connection.
+               - setting up cached data. */
+        try {
+            User user = loadUser(player.getUuid(), player.getUsername());
+            recordConnection(player.getUuid());
+            e.setProvider(new PlayerPermissionProvider(player, user, this.plugin.getContextManager().getCacheFor(player)));
+            this.plugin.getEventDispatcher().dispatchPlayerLoginProcess(player.getUuid(), player.getUsername(), user);
+        } catch (Exception ex) {
+            this.plugin.getLogger().severe("Exception occurred whilst loading data for " + player.getUuid() + " - " + player.getUsername(), ex);
+            this.plugin.getEventDispatcher().dispatchPlayerLoginProcess(player.getUuid(), player.getUsername(), null);
+        }
     }
 
     private void asyncPreLoginHandler(AsyncPlayerPreLoginEvent event) {
@@ -84,6 +138,7 @@ public class MinestomConnectionListener extends AbstractConnectionListener {
             this.plugin.getLogger().warn("User " + player.getUuid() + " - " + player.getUsername() + " doesn't have data preloaded - denying login");
             Component kickMsg = TranslationManager.render(Message.LOADING_STATE_ERROR.build());
             player.kick(kickMsg);
+            return;
         }
 
         PlayerQueryMap.initializePermissions(player, user);
